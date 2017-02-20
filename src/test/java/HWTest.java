c1d32585ee4fa8010b4e7cfb774dfd5f8cd5a643
stden <J9#m-2!k>
2017-02-20 21:30:55 +0300

    Создали HW (HelloWorld класс) и тест который запускает его main

---
+++ src/test/java/HWTest.java
@@ -0,0 +1,10 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HWTest extends Assert {
+    @Test
+    public void testHelloWorld() throws Exception {
+        HW.main();
+        //HW.main("1", "2");
+    }
+}
